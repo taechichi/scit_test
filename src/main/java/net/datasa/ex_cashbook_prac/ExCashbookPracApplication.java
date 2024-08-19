4dea48a1cd382a33563e19893db2aad86ddbbccd
taechichi <canon082828@gmail.com>
2024-08-19 19:22:24 +0900

    first commit

---
+++ src/main/java/net/datasa/ex_cashbook_prac/ExCashbookPracApplication.java
@@ -0,0 +1,13 @@
+package net.datasa.ex_cashbook_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExCashbookPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExCashbookPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/datasa/ex_cashbook_prac/ExCashbookPracApplicationTests.java
@@ -0,0 +1,13 @@
+package net.datasa.ex_cashbook_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExCashbookPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
